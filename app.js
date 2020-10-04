28c1378c7d56792e98f10b7b66a761ccd74c60a5
Henro Potgieter <henro47@gmail.com>
2020-10-05 00:09:48 +0200

    6 MongoDB & Mongoose

--- app.js
+++ app.js
@@ -1,10 +1,54 @@
 const express = require('express') ;
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders') ;
 
+mongoose.connect('mongodb+srv://admin:'+ process.env.MONGO_ATLAS_PW + '@node-rest-api.iszlv.mongodb.net/' + process.env.MONGO_ATLAS_DB +'?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) =>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','*');
+
+    if(req.method === 'OPTIONS')
+    {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+
+// Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, reg, res ,next) =>{
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
